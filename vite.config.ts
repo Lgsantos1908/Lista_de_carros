572c160afad78b75142ffa30a4ecde5b2c82cf69
lgsantos1908 <gabriellsantos1908@gmail.com>
2024-11-05 23:26:16 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lista_de_carros"
+  base: "/Lista_de_carros"
 })

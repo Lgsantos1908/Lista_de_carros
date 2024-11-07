1dcdf558e2b3c06cf4341c5e397bdd63bf1ce527
lgsantos1908 <gabriellsantos1908@gmail.com>
2024-11-07 08:01:03 -0300

    Updates

--- assets/index-CZrBNePV.js
+++ assets/index-CZrBNePV.js
@@ -96,19 +96,22 @@ Error generating stack: `+o.message+`
     height: 380px;
     padding: 1.25rem;
     margin-top: 1.56rem;
+
 `,Fm=Ot.div`
     height: 100%;
     width: 100%;  
     display: flex;
-    align-items: center;
+    align-items: start;
     justify-content: center;
     flex-direction: column;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
     
     
     h3{
         color: white;
-        margin-bottom: 0.5rem;
-        
+        margin-bottom: 0.5rem;        
     }
 `,Am=Ot.div`
     height: 100%;
@@ -116,8 +119,10 @@ Error generating stack: `+o.message+`
     border-radius: 2rem;
     box-shadow: 0 0 5px 2px rgba(50, 50, 50, 0.25);
     background-image: url('');
-    background-size: cover;
-    z-index:1;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    z-index: 1;
     display: flex;
     justify-content: center;
     

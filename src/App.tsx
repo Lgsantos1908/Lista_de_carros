160d225bb4df5e3b41df5160c9f04383538a8212
lgsantos1908 <gabriellsantos1908@gmail.com>
2024-11-09 14:42:53 -0300

    cadastro carr

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
 import  {Home}  from "./pages/Home"
+import { Cadastro} from "./pages/Cadastro"
 import { GlobalStyle } from "./styles/global"
 
 
@@ -7,8 +8,14 @@ function App() {
 
   return (
     <>
-    <GlobalStyle/>
-    <Home/>
+      <GlobalStyle/>
+      <Router>
+        <Routes>
+            <Route path="Lista_de_carros/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -2,63 +2,41 @@ import {
     Container,
     FormUser,
 } from './styles'
-// import { carrinhos } from '../../types/types';
-import {useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 export function Cadastro() {
-    // const[carrinhos, setCarrinhos]= useState<carrinhos[]>([]);
-    const[novoCarrinho, setnovoCarrinho] = useState('');
     const[nomeCarrinho, setnomeCarrinho] = useState('');
     const[colecaoCarrinho, setcolecaoCarrinho] = useState('');
 
-    // useEffect(()=> {                
-    //     fetch("https://carros-6e093-default-rtdb.firebaseio.com/carrinhos.json", {
-    //         method: "GET", 
-    //         headers: {
-    //             "Content-Type" : 'application/json'
-    //         }
-    //     })
-    //     .then(
-    //         (resp) => resp.json()
-    //         .then((data) => {
-    //             const carrinhosArray = Object.values(data) as carrinhos[];
-    //             setCarrinhos(carrinhosArray);
-    //         })                
-    //     )
-    //     .catch((err) => console.log(err))        
-    // }, []) 
 
-
-    function handleCarrinho(){
+    const handleCarrinho = () => {
         const newCarrinho = {
-            carrinhos: novoCarrinho,
-            nomeCarrinho: nomeCarrinho,
-            colecaoCarrinho: colecaoCarrinho,
+            carrinho_nome: nomeCarrinho.toUpperCase(),
+            colecao_carrinho: colecaoCarrinho.toUpperCase(),
+            foto:'',
+        };
 
-        }
+        fetch("https://carros-6e093-default-rtdb.firebaseio.com/carrinhos.json", {
+            method: "POST", 
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(newCarrinho)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data);
+            alert("Carrinho cadastrado com sucesso!");
+            // setCarrinhos([]);
+            // setnovoCarrinho('');
+            setcolecaoCarrinho('');
+            setnomeCarrinho('');
+        })
+        .catch((err) => console.log(err));
 
-        useEffect(()=> {                
-            fetch("https://carros-6e093-default-rtdb.firebaseio.com/carrinhos.json", {
-                method: "POST", 
-                headers: {
-                    "Content-Type" : 'application/json'
-                },
-                body: JSON.stringify(newCarrinho)
-            })
-            .then((resp) => resp.json())
-            .then((data) => {console.log(data)})
-            .catch((err) => console.log(err)) 
-    
-            // setCarrinhos([])
-            setnovoCarrinho('')
-            setcolecaoCarrinho('')
-            setnomeCarrinho('')
-    
-            alert("carrinho cadsatro com sucesso!")
-        }, []) 
-    }
-
+        
+    };     
    
 
     return(
@@ -69,7 +47,7 @@ export function Cadastro() {
                 <h2>Nome</h2>
                 <input type="text" onChange={(e) => setnomeCarrinho(e.target.value)} />
                 <h2>Coleção</h2>
-                <input type="password" onChange={(e) => setcolecaoCarrinho(e.target.value)}/>
+                <input type="text" onChange={(e) => setcolecaoCarrinho(e.target.value)}/>
                 <Link to='/home'>
                     <input type="submit" value='Criar carrinho' onClick={handleCarrinho}/>
                 </Link>

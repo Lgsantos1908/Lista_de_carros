4a1b9c711173c8a416e5913523390769298df9f1
lgsantos1908 <gabriellsantos1908@gmail.com>
2025-03-25 20:02:24 -0300

    ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,37 @@ import { Cadastro} from "./components/Cadastro";
 import { GlobalStyle } from "./styles/global";
 
 
-function App() {
+ function App() {
 
-  return (
-    <>
-      <GlobalStyle/>
-      <Router basename="/Lista_de_carros">
-        <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cadastro" element={<Cadastro/>} />
-            <Route path="/login" element={<Login/>}/>
-        </Routes>
-      </Router>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <GlobalStyle/>
+//       <Router basename="/Lista_de_carros">
+//         <Routes>
+//             <Route path="/" element={<Navigate to="/home" />} />
+//             <Route path="/home" element={<Home />} />
+//             <Route path="/cadastro" element={<Cadastro/>} />
+//             <Route path="/login" element={<Login/>}/>
+//         </Routes>
+//       </Router>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
+return (
+  <>
+    <GlobalStyle />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  </>
+);
+ }
+
+export default App;

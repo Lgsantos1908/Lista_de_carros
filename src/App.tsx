ae62783741c98bb1af73249a9ebf9e41282ed886
lgsantos1908 <gabriellsantos1908@gmail.com>
2025-04-16 10:56:32 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter  as Router, Route, Routes, Navigate } from "react-router-dom"
+import { Router, Route, Routes, Navigate } from "react-router-dom"
 import  {Home}  from "./pages/Home";
 import { Login } from "./components/Login";
 import { Cadastro} from "./components/Cadastro";
@@ -7,34 +7,19 @@ import { GlobalStyle } from "./styles/global";
 
  function App() {
 
-//   return (
-//     <>
-//       <GlobalStyle/>
-//       <Router basename="/Lista_de_carros">
-//         <Routes>
-//             <Route path="/" element={<Navigate to="/home" />} />
-//             <Route path="/home" element={<Home />} />
-//             <Route path="/cadastro" element={<Cadastro/>} />
-//             <Route path="/login" element={<Login/>}/>
-//         </Routes>
-//       </Router>
-//     </>
-//   )
-// }
+  return (
+    <>
+      <GlobalStyle/>
+      <Router basename="/Lista_de_carros">
+        <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro/>} />
+            <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
 
-// export default App
-return (
-  <>
-    <GlobalStyle />
-    <Router>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  </>
-);
- }
-
-export default App;
+export default App
